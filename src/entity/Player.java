51ea835b28dd82e61eadfb2ed6da5cb85248bed3
Yehor-Kotliar <egorkotlyar44@icloud.com>
2025-08-03 20:38:39 +0200

    Implement collision detection

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -7,6 +7,7 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.Objects;
 
 public class Player extends Entity{
 
@@ -46,14 +47,14 @@ public class Player extends Entity{
 
         try {
 
-            up1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png"));
-            up2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_2.png"));
-            down1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_1.png"));
-            down2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_2.png"));
-            left1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_1.png"));
-            left2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_2.png"));
-            right1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_1.png"));
-            right2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_2.png"));
+            up1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_up_1.png")));
+            up2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_up_2.png")));
+            down1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_down_1.png")));
+            down2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_down_2.png")));
+            left1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_left_1.png")));
+            left2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_left_2.png")));
+            right1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_right_1.png")));
+            right2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_right_2.png")));
 
         } catch (IOException e) {
 
@@ -68,23 +69,32 @@ public class Player extends Entity{
 
             if(keyH.upPressed){
                 direction = "up";
-                worldY -= speed;
             }
             else if(keyH.downPressed){
                 direction = "down";
-                worldY += speed;
             }
             else if(keyH.leftPressed){
                 direction = "left";
-                worldX -= speed;
             }
             else if(keyH.rightPressed){
                 direction = "right";
-                worldX += speed;
             }
 
+            // CHECK TILE COLLISION
             collisionOn = false;
-            gp.cCHecker.checkTie(this);
+            gp.cCHecker.checkTile(this);
+
+            //IF COLLISION IS FALSE PLAYER CAN MOVE
+            if(collisionOn == false) {
+
+                switch (direction) {
+                    case "up": worldY -= speed; break;
+                    case "down": worldY += speed; break;
+                    case "left": worldX -= speed; break;
+                    case "right": worldX += speed; break;
+                }
+            }
+
 
             spriteCounter++;
             if(spriteCounter > 15) {
